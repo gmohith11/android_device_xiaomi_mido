d8ec30b74df40039daddf32ff0b511a27240cecd
gmohith11 <gmohith11@gmail.com>
2017-09-10 23:50:47 +0530

    Update and rename cm.mk to dot.mk

--- dot.mk
+++ dot.mk
@@ -17,9 +17,9 @@
 $(call inherit-product, device/xiaomi/mido/full_mido.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_mido
+PRODUCT_NAME := dot_mido
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
